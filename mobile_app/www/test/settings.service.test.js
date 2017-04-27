9a653599830ad587e345c1a7c49f13b6ed672da3
adamo <mosca125@gmail.com>
2017-04-27 10:29:34 -0400

    added test for

--- mobile_app/www/test/settings.service.test.js
+++ mobile_app/www/test/settings.service.test.js
@@ -7,9 +7,11 @@ describe('Settings service tests', function() {
 
     beforeEach(function() {
         module(function($provide) {
-            //$provide.factory
+            $provide.service('$interval', function() {
+                this.cancel = jasmine.createSpy('cancel');
+            })
         });
-        module('core.settings');
+        module('core.settings')
     });
 
 	beforeEach(inject(function($resource, $timeout, $interval, $location, Settings) {
@@ -24,4 +26,17 @@ describe('Settings service tests', function() {
 		mockSettings.setRoundTime(1000);
         expect(mockSettings.getRoundTime()).toBe(1000);
 	});
+
+    it('should cancel the timer', function() {
+        mockSettings.cancelInterval(function() {});
+        expect(mockInterval.cancel).toHaveBeenCalled();
+    });
+
+    it('should set an interval', function() {
+        //expect(mockSettings.interval(() => {}, 1000,1).then).toBeDefined();
+    });
+
+    it('should set a sleep timer', function() {
+        expect(mockSettings.sleep(() => {}, 1000).then).toBeDefined();
+    })
 });
